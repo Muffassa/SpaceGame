38d3d09ffdb5b0467cfb11d69566f6c4246e4323
Nikita <keddofilatov@gmail.com>
2016-04-07 19:00:06 +0600

    Space Game

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+
+        frame.setTitle("SpaceGame");
+        frame.setSize(100, 200);
+
+        frame.setVisible(true)  ;
+    }
+}
